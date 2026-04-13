0e04600e3ee2b3f2f4e791f8fef62ef0f4beb6cb
David Gomes <davisgomes2003@gmail.com>
2026-04-13 11:46:37 -0700

    Monday, April 4th wokr

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -1,22 +1,52 @@
 /**
  * Unit Tests for Password class
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "Password.h"
 
 class PasswordTest : public ::testing::Test
 {
-	protected:
-		PasswordTest(){} //constructor runs before each test
-		virtual ~PasswordTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+protected:
+	PasswordTest() {}		   // constructor runs before each test
+	virtual ~PasswordTest() {} // destructor cleans up after tests
+	virtual void SetUp() {}	   // sets up before each test (after constructor)
+	virtual void TearDown() {} // clean up after each test, (before destructor)
 };
-
+/*
 TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+}
+
+TEST(PasswordTest, multi_letter_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("ZZz"));
+}
+/*
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters(""));
+}
+*/
+
+TEST(PasswordTest, mixed_case_valid)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("aB"));
+}
+
+TEST(PasswordTest, mixed_case_all_lower)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("abcd"));
+}
+
+TEST(PasswordTest, mixed_case_all_upper)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("ABCD"));
 }
